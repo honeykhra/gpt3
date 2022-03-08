faf4fc0af0f2c26edd0b0f8d63d51832dfa9429e
honeykhra <honeykhra@gmail.com>
2022-03-08 17:04:51 +0530

    created folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { Article } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Gpt 3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
