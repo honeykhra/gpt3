038971ef60bb90eac08409c5035d956c66c49b2a
honeykhra <honeykhra@gmail.com>
2022-03-08 17:13:36 +0530

    assets and logo import

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./navbar.css";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo"></div>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="" />
+        </div>
       </div>
     </div>
   );
