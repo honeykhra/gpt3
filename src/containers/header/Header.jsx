f84656e67e6a310431ac882d07bd2032b1658be2
honeykhra <honeykhra@gmail.com>
2022-03-09 19:30:49 +0530

    p add

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -22,6 +22,7 @@ const Header = () => {
         </div>
         <div className="gpt3__header-content__people">
           <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
     </div>
