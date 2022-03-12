c35189b4b2f931f0897d3dc0868b1718b13573cc
honeykhra <honeykhra@gmail.com>
2022-03-12 20:35:48 +0530

    whatgpt3 started

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './whatGPT3.css'
+import React from "react";
+import { Feature } from "../../components";
+import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
-  )
-}
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-features">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
