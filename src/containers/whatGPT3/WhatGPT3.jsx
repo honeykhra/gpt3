58c40878d620b7db6da321be8a939223fd20989b
honeykhra <honeykhra@gmail.com>
2022-03-19 16:37:47 +0530

    features completed

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-features">
         <Feature
           title="What is GPT-3"
