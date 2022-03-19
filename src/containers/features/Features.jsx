5715afe9aa2c1d6dbbcd15f160538cd3b0fdc3f3
honeykhra <honeykhra@gmail.com>
2022-03-19 16:28:32 +0530

    mistake in name

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -24,14 +24,12 @@ const featuresData = [
 const Features = () => {
   return (
     <div className="gpt3__features section__padding">
-      <div className="gp3__features-heading">
-        <h1 className="gradient__text gpt3__features-heading-h1">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
           The Future is Now and You Just Need To Realize It. Step into Future
           Today & Make it Happen.
         </h1>
-        <p className="gpt3__features-heading-p">
-          Request Early Access to Get Started
-        </p>
+        <p>Request Early Access to Get Started</p>
       </div>
       <div className="gpt3__features-container">
         {featuresData.map((item, index) => (
