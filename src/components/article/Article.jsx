abe76aa9093856872afffbb8e7bd9f59fde24afa
honeykhra <honeykhra@gmail.com>
2022-03-20 21:02:06 +0530

    blog started

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './article.css'
+import React from "react";
+import "./article.css";
 
-const Article = () => {
+const Article = ({ imgUrl }) => {
   return (
-    <div>Article</div>
-  )
-}
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+    </div>
+  );
+};
 
-export default Article
+export default Article;
