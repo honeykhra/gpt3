6e0c2013e06df94145033441d55cc350579a8699
honeykhra <honeykhra@gmail.com>
2022-03-23 20:28:02 +0530

    footer jsx started

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="gpt3__footer section_padding">
+      <div className="gpt3__footer-heading">
+        <h2>Do you want to step in to the future before others</h2>
+      </div>
+      <div className="gpt3__footer-btn">Request Early Access</div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
