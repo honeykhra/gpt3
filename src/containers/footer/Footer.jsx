94abd98ebc87a39a6811b90febdae275fff88c8d
honeykhra <honeykhra@gmail.com>
2022-03-23 20:47:02 +0530

    footer jsx done

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.css";
+import gpt3logo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
@@ -8,6 +9,34 @@ const Footer = () => {
         <h2>Do you want to step in to the future before others</h2>
       </div>
       <div className="gpt3__footer-btn">Request Early Access</div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        © 2021 GPT-3. All rights reserved.
+      </div>
     </div>
   );
 };
